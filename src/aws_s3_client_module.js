61c5b4eb635169117b5bee737e566ffe7111d331
Arun Bose <arun@core.ai>
2022-02-08 13:08:08 +0530

    fix: filename was being passed instead of file path causing failures when the file was not in current dir

--- src/aws_s3_client_module.js
+++ src/aws_s3_client_module.js
@@ -7,19 +7,19 @@ import stream from "stream";
 const {S3, Endpoint} = pkg;
 
 /**
- * Module to upload a file to AWS S3/Linode Object Storage. The path of the file to be uploaded is
+ * Module to upload a filePath to AWS S3/Linode Object Storage. The path of the filePath to be uploaded is
  * passed on as a parameter which is then processed and uploaded using AWS S3 client.
  *
  * @param accessKeyId bucket specific unique identifier required for authentication
  * @param secretAccessKey user specific unique identifier required for authentication
  * @param region indicates the geographical server location (e.g us-east-1, eu-west-1a)
- * @param file complete path of the file to be uploaded is passed on as a parameter
- * @param bucket uniquely identifies the bucket where the file should be uploaded
- * @param url suffix url to decide whether to upload the file to AWS S3 or LiNode Object Storage
+ * @param filePath complete path of the filePath to be uploaded is passed on as a parameter
+ * @param bucket uniquely identifies the bucket where the filePath should be uploaded
+ * @param url suffix url to decide whether to upload the filePath to AWS S3 or LiNode Object Storage
  * @returns {Promise<void>} void
  */
 
-async function uploadFileToBucket (accessKeyId, secretAccessKey, region, file, bucket, url) {
+async function uploadFileToBucket (accessKeyId, secretAccessKey, region, filePath, bucket, url) {
     const uploadStream = ({ Bucket, Key }) => {
         const s3Client = new S3({
             accessKeyId: accessKeyId,
@@ -34,8 +34,8 @@ async function uploadFileToBucket (accessKeyId, secretAccessKey, region, file, b
             promise: s3Client.upload({ Bucket, Key, Body: pass }).promise()
         };
     };
-    const filePath = path.basename(file);
-    const { writeStream, promise } = uploadStream({Bucket: bucket, Key: filePath});
+    const fileName = path.basename(filePath);
+    const { writeStream, promise } = uploadStream({Bucket: bucket, Key: fileName});
     fsExtra.createReadStream(filePath).pipe(writeStream);
     const response = {
         status: true,
